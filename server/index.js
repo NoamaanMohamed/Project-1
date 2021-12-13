653c27209a6f2f452c379828422af7c40e02f244
Julian James <julian@Julians-MacBook-Pro.local>
2021-12-13 16:43:33 +0000

    changed port in index.js

--- server/index.js
+++ server/index.js
@@ -1,2 +1,3 @@
 const server = require("./app")
-server.listen(3000, () => console.log(`localhost:3000`))
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`localhost:3000`));
