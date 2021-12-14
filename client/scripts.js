397a8efbe8134edd992098d4fd706dbe6887f380
“Ginger9307” <tvlezhneva@gmail.com>
2021-12-14 02:37:02 +0000

    front + end

--- client/scripts.js
+++ client/scripts.js
@@ -1,32 +1,162 @@
 
-const showBtn = document.querySelector("#showAll");
-const postBtn = document.querySelector("#postBtn");
-showBtn.addEventListener('click', showAll);
-postBtn.addEventListener('click', sendPost);
+// const showBtn = document.querySelector("#showAll");
+// const postBtn = document.querySelector("#postBtn");
+// showBtn.addEventListener('click', showAll);
+// postBtn.addEventListener('click', sendPost);
 
+
+showAll();
+
+document.addEventListener("click", function(e) {
+  // console.log(e.target);
+  // console.log(e.target.className);
+  if((e.target && e.target.className == "postFrame")   || 
+      (e.target && e.target.className == "post")       ||
+      (e.target && e.target.className == "postFooter") ||
+      (e.target && e.target.className == "postTitle")  || 
+      (e.target && e.target.className == "postBody")) {
+      console.log(e.target.className)
+      console.log(e.target);
+
+      let selectedPostID = e.target.getAttribute('data-id');
+      console.log(selectedPostID);
+      showPostAndComments(selectedPostID );
+  }
+});
+
+// -----------------FUNCTIONS----------------------
 function showAll() {
   fetch('http://localhost:3000/posts')
     .then(resp => resp.json())
     .then(appendPosts).catch(console.warn);
 }
 
-function appendPosts(posts){
-  posts.forEach(addAllPosts);
+// ----replaced by ginger
+// function appendPosts(posts){
+//   posts.forEach(addAllPosts);
+// };
+
+// function addAllPosts(postData) {
+//   const newDiv = document.createElement('div');
+//   newDiv.className = "col-sm";
+//   newDiv.textContent = `${postData.body}`;
+//   const element = document.querySelector("div.row");
+//   element.appendChild(newDiv);
+// }
+function appendPosts(posts) {
+  posts.forEach(showPost);
 };
 
-function addAllPosts(postData) {
-  const newDiv = document.createElement('div');
-  newDiv.className = "col-sm";
-  newDiv.textContent = `${postData.content}`;
-  const element = document.querySelector("div.row");
-  element.appendChild(newDiv);
+function showPost(post) {
+  console.log(post);
+
+  const postGrid = document.querySelector(".allPosts");
+  console.log(postGrid);
+
+  const newPost = document.createElement('div');
+  newPost.classList.add('col-md-4');
+  newPost.classList.add('col-sm-6');
+  newPost.classList.add('col-xs-12');
+  newPost.classList.add('post');
+  newPost.setAttribute('data-id', post.id);
+  postGrid.append(newPost);
+
+  const newPostFrame = document.createElement('div');
+  newPostFrame.classList.add('postFrame');
+  newPostFrame.setAttribute('data-id', post.id);
+  console.log(newPostFrame.getAttribute("data-id"));
+  newPost.append(newPostFrame);
+
+  const newPostTitle = document.createElement('h3');
+  newPostTitle.innerText = post.title;
+  newPostTitle.setAttribute('data-id', post.id);
+  newPostTitle.classList.add('postTitle');
+  newPostFrame.append(newPostTitle);
+
+  const newPostBody = document.createElement('p');
+  newPostBody.setAttribute('data-id', post.id);
+  newPostBody.innerText = post.body;
+  newPostBody.classList.add('postBody');
+  newPostFrame.append(newPostBody);
+
+  const newPostFooter = document.createElement('div');
+  newPostFooter.setAttribute('data-id', post.id);
+  newPostFooter.classList.add('postFooter');
+  newPostFrame.append(newPostFooter);
+
+  const newComNumber = document.createElement('p');
+  // open with comments
+  // newComNumber.innerHTML = `<i class="fas fa-comment"></i> ${post.comments.length} `
+  newComNumber.innerHTML = `<i class="fas fa-comment"></i> `
+  newComNumber.classList.add('card-text');
+  newComNumber.setAttribute('data-id', post.id);
+  // newComNumber.innerHTML = post.comments.length;
+  newPostFooter.append(newComNumber);
+
+  // const newCommentSign = document.createElement('i');
+  // newCommentSign.classList.add('fas');
+  // newCommentSign.classList.add('fa-comment');
+  // newComNumber.append(newCommentSign);         
+};
+
+// added by ginger
+function showPostAndComments(postId) {
+  document.getElementById('posts').classList.add('hide-section');
+  document.getElementById('showPostAndComments').classList.remove('hide-section');
+
+  const post = posts[postId];
+  console.log(post);
+
+  const singlePost = document.querySelector('.singlePost')
+
+  const newPostTitle = document.createElement('h3');
+  // open after title
+  // newPostTitle.innerText = post.title;
+  newPostTitle.classList.add('postTitle');
+  singlePost.append(newPostTitle);
+
+  const newPostBody = document.createElement('p');
+  // open after API by ID
+  // newPostBody.innerText = post.body;
+  newPostBody.classList.add('postBody');
+  singlePost.append(newPostBody);
+
+  const newPostFooter = document.createElement('div');
+  // open after API by ID
+  // newPostFooter.setAttribute('data-id', post.id);
+  newPostFooter.classList.add('postFooter');
+  singlePost.append(newPostFooter);
+
+  const newComNumber = document.createElement('p');
+  // newComNumber.innerHTML = `<i class="fas fa-comment"></i> ${post.comments.length} `
+  newComNumber.innerHTML = `<i class="fas fa-comment"></i>  `
+  newComNumber.classList.add('card-text');
+  // open after API by ID
+  // newPostFooter.setAttribute('data-id', post.id);
+  newPostFooter.append(newComNumber);
+  // open after API by ID
+  // console.log(post.comments);
+
+  // post.comments.forEach(comment => showComment(comment));
 }
+// open after API by ID
+// function showComment(comment) {
+//   const newComFrame = document.createElement('div');
+//   newComFrame.classList.add('comFrame');
+//   document.querySelector('.comList').append(newComFrame);
+
+//   const newComment = document.createElement('p');
+//   newComment.innerHTML = comment.body;
+//   newComment.classList.add('comment');
+//   newComFrame.append(newComment);
+// }
+
 
 function sendPost(e){
   e.preventDefault();
 
   const postData = {
-    content: e.target.content.value,
+    body: e.target.body.value,
     date : new Date().toJSON().slice(0, 10),
   };
   const options = { 
@@ -43,8 +173,6 @@ function sendPost(e){
   .catch(console.warn);
 };
 
-
-showAll();
 
 const giphyForm = document.querySelector('#giphy-form');
             let ApiKey = "XVDNoMMkh34V76bFFB0HhvT9SJiQJim8";
@@ -72,3 +200,5 @@ const giphyForm = document.querySelector('#giphy-form');
                     )
             }
             giphyForm.addEventListener("submit", sendApiRequest);
+
+
